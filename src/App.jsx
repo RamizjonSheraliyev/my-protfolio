2ea7273faf585fc30fc295c9cc32585a471ab953
ramizjo <ramizsheraliyev8@gmail.com>
2025-02-02 16:53:39 +0500

    git commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import SecureForm from "./components/about";
-import About from "./components/aboutform";
-import Home from "./components/home";
-import Header from "./components/header";
+// import About from "./components/aboutform";
+// import Home from "./components/home";
+// import Header from "./components/header";
 import Footer from "./components/navbar.jsx";
 // import TestModeNotification from "./components/test";
 
 function App() {
   return <>
   {/* <TestModeNotification/> */}
-  <Header/>
-  <Home/>
-  <About/>
+  {/* <Header/> */}
+  {/* <Home/> */}
+  {/* <About/> */}
   <SecureForm/>
   <Footer/>
   </>;
