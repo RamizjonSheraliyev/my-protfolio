b6117da606886bc0dabf42e6dd963628aa01bb26
ramizjo <ramizsheraliyev8@gmail.com>
2025-02-02 17:32:53 +0500

    loading add

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
+import  { useState, useEffect } from "react";
 import "./App.css";
 import SecureForm from "./components/about";
-// import About from "./components/aboutform";
-// import Home from "./components/home";
-// import Header from "./components/header";
 import Footer from "./components/navbar.jsx";
-// import TestModeNotification from "./components/test";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 function App() {
-  return <>
-  {/* <TestModeNotification/> */}
-  {/* <Header/> */}
-  {/* <Home/> */}
-  {/* <About/> */}
-  <SecureForm/>
-  <Footer/>
-  </>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a 2-second delay for loading
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup the timer on unmount
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <SecureForm />
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
